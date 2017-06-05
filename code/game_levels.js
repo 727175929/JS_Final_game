4f34781f39b7e0288fff04967d266a8ea9966107
727175929 <727175929@qq.com>
2017-06-05 19:30:39 +0800

    理解原来的小例子并且添加代码注释，接下来开始进行CSS样式的改变和人物运动的动画添加

--- code/game_levels.js
+++ code/game_levels.js
@@ -1,29 +1,18 @@
 var GAME_LEVELS = [
-  ["                                                                                ",
-   "                                                                                ",
-   "                                                                                ",
-   "                                                                                ",
-   "                                                                                ",
-   "                                                                                ",
-   "                                                                  xxx           ",
-   "                                                   xx      xx    xx!xx          ",
-   "                                    o o      xx                  x!!!x          ",
-   "                                                                 xx!xx          ",
-   "                                   xxxxx                          xvx           ",
-   "                                                                            xx  ",
    "  xx                                      o o                                x  ",
-   "  x                     o                                                    x  ",
-   "  x                                      xxxxx                             o x  ",
-   "  x          xxxx       o                                                    x  ",
-   "  x  @       xx                                                 xxxxx       x   ",
-   "  xxxxxxxxxxxx  xxxxxxxxxxxxxxx   xxxxxxxxxxxxxxxxxxxx     xxxxxxx   xxxxxxxxx  ",
-   "                              x   x                  x     x                    ",
-   "                              x!!!x                  x!!!!!x                    ",
-   "                              x!!!x                  x!!!!!x                    ",
-   "                              xxxxx                  xxxxxxx                    ",
-   "                                                                                ",
-   "                                                                                "],
+  [
+  "                      ",
+  "                      ",
+  "  x              = x  ",
+  "  x         o o    x  ",
+  "  x @      xxxxx   x  ",
+  "  xxxxx            x  ",
+  "      x!!!!!!!!!!!!x  ",
+  "      xxxxxxxxxxxxxx  ",
+  "                      "
+]
 ];
+
 
 if (typeof module != "undefined" && module.exports)
   module.exports = GAME_LEVELS;
